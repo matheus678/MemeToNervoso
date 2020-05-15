acc7c7eb0b818f111e6021c5c2e21564cf3ddfc2
matheus678 <60980352+matheus678@users.noreply.github.com>
2020-05-15 20:33:12 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace MemeToNervoso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Usuário, o que você pensa sobre mim? Você é muito sem graça!");
+            Console.WriteLine("- E sabe o que eu penso de você?");
+            string caractere = Console.ReadLine();
+            caractere = caractere.Remove(10);
+            Console.WriteLine(caractere + "... NÃO, PERA.\nTô nervoso");
+
+
+
+
+        }
+    }
+}
